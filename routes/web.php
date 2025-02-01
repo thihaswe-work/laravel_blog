0e1ae6eb61eff704fc44ec33bba8afb6a84cc030
Macmini <macmini@Macminis-Mac-mini.local>
2025-02-01 13:34:05 +0630

    crd

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Product\ProductController;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ArticleController::class, 'index']);
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+Route::get('/articles/add', [ArticleController::class, 'add']);
+Route::post('/articles/add', [
+    ArticleController::class,
+    'create'
+]);
+Route::get('/articles/delete/{id}', [
+    ArticleController::class,
+    'delete'
+]);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
